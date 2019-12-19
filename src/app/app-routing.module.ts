5e7998ea406abe9eb69bd0d2a3d42bfbd143b887
Hess <jonathan.hess@accenturefederal.com>
2019-12-19 13:50:52 -0500

    stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 	{ path: 'posts', loadChildren: () => import('./post/post.module').then(m => m.PostModule) },
+	{ path: 'search', loadChildren: () => import('./search/search.module').then(m=> m.SearchModule) },
 	{ path: 'profile', loadChildren: () => import('./user-profile/user-profile.module').then(m => m.UserProfileModule) },
 	{ path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
 ];
