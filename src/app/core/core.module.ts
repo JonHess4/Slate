260d0d50ebb1fa7a65d6b5a1d769e3ebd4c37d09
Hess <jonathan.hess@accenturefederal.com>
2020-01-03 13:45:16 -0500

    deleting most navbar stuff

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarComponent } from './components/nav-bar.component';
+import { NavBarComponent } from './components/navbar.component';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer.component';
 import { HttpClientModule } from '@angular/common/http';
